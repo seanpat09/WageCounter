25d8e4a545ee4ab550ae4dbaeeb4c3d2569523be
Sean Cuevo <seanpat09@gmail.com>
2012-06-20 20:18:38 -0400

    Styled counter

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function UpdateCounter(initial_wage, accumulated)
-{
-  return accumulated + initial_wage;
-}
-
 $(document).ready(function() {
-  var initial_wage = parseFloat($("div#display_counter p:last").text());
-  var accumulated = 0;
+  var initial_wage = parseFloat($(".display").text());
+  var accumulated = parseFloat($(".display").text());;
   setInterval(function(){
-    accumulated = parseFloat($("div#display_counter p:last").text());
-    $("div#display_counter p:last"); 
-    $("div#display_counter p:last").text(UpdateCounter(initial_wage, accumulated));
+    accumulated += initial_wage;
+    $(".display").text("$" + accumulated.toPrecision(4));
     }, 1000);
 })
